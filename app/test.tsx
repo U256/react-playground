2dae3c201852410c45aa1c0440d9434f286540b6
A. Chernavtsev <chernavtsev.av@gmail.com>
2023-04-03 14:34:49 +0300

    feat: replace vite with nextjs after repo rename

---
+++ app/test.tsx
@@ -0,0 +1,21 @@
+import { FC, ReactNode, useState } from 'react'
+
+interface Props {
+	children?: ReactNode
+}
+
+export const TestPage: FC<Props> = ({ children }) => {
+	const [state, setState] = useState(0)
+
+	return (
+		<>
+			{children}
+			<h1>Test Page</h1>
+			<p>state: {state}</p>
+			<p>This is a test page</p>
+			<button type="button" onClick={() => setState((p) => p + 1)}>
+				increase
+			</button>
+		</>
+	)
+}
--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-// import viteCompression from 'vite-plugin-compression'; // OR vite-plugin-compress ?
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // plugins: [viteCompression()],
-})
