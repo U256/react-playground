0b0103a83461d44267e4a36aca10abe54afe43d3
A. Chernavtsev <chernavtsev.av@gmail.com>
2023-04-04 16:51:46 +0300

    feat: modular architecture

--- .eslintrc.js
+++ .eslintrc.js
@@ -45,6 +45,7 @@ module.exports = {
 		'react/jsx-indent-props': [2, 'tab'],
 		'react/jsx-indent': 0,
 		'react/jsx-key': 'error',
+		'react/jsx-props-no-spreading': 0,
 		'react/require-default-props': 0,
 		'@next/next/no-html-link-for-pages': 0,
 
